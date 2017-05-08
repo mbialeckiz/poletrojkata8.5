4488709d4d22ca1ea8803dbf1bdfdf1162f42ad9
mbialeckiz <marcinbialeckiz@gmail.com>
2017-05-08 10:20:30 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var a,h, triangleArea;
+
+a= prompt("Wpisz wartość a");
+h= prompt("Wpisz wartość h");
+
+triangleArea = a*h/2
+
+
+console.log(triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea)
+
